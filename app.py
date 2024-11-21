c8b41ebe1898a4777d133cb9d8a045547d90077f
evangelostsak <baggtsak55@gmail.com>
2024-11-21 03:00:11 +0100

    loading posts from json file added

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
+def load_posts():
+    with open('blog_posts.json', 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
