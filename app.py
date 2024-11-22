cc55a87b5b9b7def84d72cbfde937c0cb8240c8e
evangelostsak <baggtsak55@gmail.com>
2024-11-22 02:43:06 +0100

    Update: Add post to blog included.

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
 
 
 def load_posts():
-    with open('blog_posts.json', 'r', encoding='utf-8') as file:
-        return json.load(file)
+    try:
+        with open('blog_posts.json', 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+
+def save_posts(posts):
+    with open('blog_posts.json', 'w', encoding='utf-8') as file:
+        return json.dump(posts, file, indent=4)
 
 
 @app.route('/')
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        blog_posts = load_posts()
+
+        id_gen = max((post['id'] for post in blog_posts), default=0) + 1
+        new_post = {
+            'id': id_gen,
+            'author': request.form.get('author'),
+            'title': request.form.get('title'),
+            'content': request.form.get('content')
+        }
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
